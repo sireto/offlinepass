f0dc4897a054e9b0af64994ed0dbb6364b259d75
Niraj <duwalkneerose@gmail.com>
2022-12-28 00:14:15 +0545

    remove hmac function

--- web/src/containers/BannerContainer.tsx
+++ web/src/containers/BannerContainer.tsx
@@ -7,19 +7,6 @@ import bs58 from "bs58";
 
 import { encryptedConstants, offlineConstants, recoverableConstant } from "@app/constants/benifits";
 export default function BannerContainer() {
-  const hmacSha256 = () => {
-    const msg = "facebook.com | abc@gmail.com | 2022-01-01 | 0"; // Uint8Array of arbitrary length
-    const keyString = "H@rdPassw0rd"; // Uint8Array of 32 bytes (since SHA-256 is used)
-    const hash = "SHA-256";
-    const retries = 0;
-    const keyUnit8Array = new TextEncoder().encode(keyString);
-    const msgUnit8Array = new TextEncoder().encode(msg);
-    hmac.compute(keyUnit8Array, msgUnit8Array, hash).then((mac: any) => {
-      // now you get a keyed-hash of msg in Uint8Array
-      console.log(`${retries}$${bs58.encode(mac)}`);
-      //   console.log(new TextDecoder("utf-8").decode(mac));
-    });
-  };
 
   const BannerDetails = () => (
     <div className="space-y-14">
@@ -38,7 +25,7 @@ export default function BannerContainer() {
         <Image src={BannerImage} alt={""} height={1000} width={400} />
         <div className="space-y-8">
           <BannerDetails />
-          <Button onClick={hmacSha256} className="text-semibold">
+          <Button className="text-semibold">
             Get Started
           </Button>
         </div>
