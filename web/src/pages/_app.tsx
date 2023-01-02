d7a1f63ff0a8f58194c2e88e677f78b89c9df01e
Niraj <duwalkneerose@gmail.com>
2023-01-02 11:50:39 +0545

    Add google analytics,Cookies

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -8,13 +8,32 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import Head from "next/head";
 import store, { persistor } from "@app/store/store";
-
+import Script from "next/script";
+import Link from "next/link";
+import CookieConsent from "react-cookie-consent";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>OfflinePass</title>
       </Head>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-FGMGC6TQ6L"
+      ></script>
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-FGMGC6TQ6L');
+        `,
+        }}
+      />
       <ToastContainer
         theme="colored"
         position="bottom-right"
@@ -34,6 +53,21 @@ export default function App({ Component, pageProps }: AppProps) {
           </PersistGate>
         </Provider>
       </Layout>
+      <CookieConsent
+        location="bottom"
+        cookieName="OfflinePassCookies"
+        expires={999}
+        buttonStyle={{
+          marginRight: 90,
+        }}
+      >
+        This website uses cookies to enhance the user experience.
+        <Link href="https://www.termsfeed.com/blog/cookies/">
+          <a className=" text-blue-500 pl-3  " target="_blank">
+            Learn More
+          </a>
+        </Link>
+      </CookieConsent>
     </>
   );
 }
