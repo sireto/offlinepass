7b9f3336b61f391e6e3b21f3b531cc27488efb9a
Niraj <duwalkneerose@gmail.com>
2023-01-03 11:26:31 +0545

    Add pagePath on gtag

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -33,7 +33,9 @@ export default function App({ Component, pageProps }: AppProps) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-FGMGC6TQ6L');
+          gtag('config', 'G-FGMGC6TQ6L', {
+              page_path: window.location.pathname,
+            });
         `,
         }}
       />
