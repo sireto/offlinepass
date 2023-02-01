1a501bde737050c32d0917fa4145a14696df5ecd
Niraj <duwalkneerose@gmail.com>
2023-02-01 08:19:16 +0545

    remove formcontainer renderformcontent  function

--- web/src/components/form-views/container.tsx
+++ web/src/components/form-views/container.tsx
@@ -1,19 +1,15 @@
 import GeneratePasswordView from "@app/components/form-views/generate-password-view";
 import cn from "classnames";
 
-function renderFormContent() {
-  return <GeneratePasswordView />;
-}
-
 export default function FormContainer({ className }) {
   return (
     <div
       className={cn(
-        "flex flex-col  justify-center w-full h-full items-end",
+        "flex flex-col  justify-center h-full lg:w-auto w-full items-end",
         className
       )}
     >
-      <div className="lg:w-auto w-full h-min">{renderFormContent()}</div>
+      <GeneratePasswordView />
     </div>
   );
 }
